12b9639dccf0abe20d14019c1ab07bd7320cc333
idekferewf <st50@WS50-09>
2025-09-18 08:59:23 +0300

    Создали текстовый массив с тремя элементами

---
+++ arrrrey/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrrrey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[3] {"pac","git","tic"};
+           
+        }
+    }
+}
