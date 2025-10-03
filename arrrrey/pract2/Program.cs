b9ee989a85f91d7e7c61a6c79e0241613e7b7b6c
lubavablin <lubovzencenko098@gmail.com>
2025-10-03 23:48:17 +0300

    рандом массив

---
+++ arrrrey/pract2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n = 4000;
+            int[] num = new int[n];
+            /// Заполнение массива случайными числами от 1 до 5000
+            for (int i = 0; i < n; i++)
+            {
+                num[i] = random.Next(1, 5001);
+            }
+            /// Количество пар находим
+            int pairsCount = n / 2 - 1;
+            int[] sum = new int[pairsCount];
+            for (int i = 0, j = 0; i < pairsCount; i++, j += 2)
+            {
+                sum[i] = num[i] + num[j + 2];
+            }
+            /// выводим результат
+            Console.WriteLine("[" + string.Join(",", sum) + "]");
+        }
+    }
+}
