6374ba91712631a6dcaa01d8d5c1868ee143e6e7
Z <pgbc303@gmail.com>
2026-01-16 10:23:06 +0300

    добавили столбцы

---
+++ PropertiesApp/PropertiesWinForms/PropertiesWinForms/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace PropertiesWinForms
+{
+    public class Student
+    {
+        private string name_;
+        private DateTime birthDay_;
+        private string number_;
+        private double avgBall_;
+
+        [DisplayName("ФИО")]
+        public string Name
+        {
+            get { return name_; }
+            set { name_ = value; }
+        }
+
+        [DisplayName("Дата рождения")]
+        public string BirthDay
+        {
+            get { return birthDay_.ToString("dd.mm.yyyy"); }
+        }
+        public void SetBirthDay(DateTime d)
+        {
+            birthDay_ = d;
+        }
+        [DisplayName("Средний балл")]
+        public double AVGBall
+        {
+            get { return avgBall_; }
+            set { avgBall_ = value; }
+        }
+        [DisplayName("Студенческий билет")]
+        public string Number
+        {
+            get { return number_; }
+            set { number_ = value; }
+        }
+    }
+}
